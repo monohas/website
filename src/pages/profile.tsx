95f1994c540f515187e843f2b0bef6e2d1c14a28
muta yuki <mutta@mutanoMacBook-Air.local>
2024-09-05 01:41:56 +0900

    profil;2B;2Aeバグ修正

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,5 +1,4 @@
 import Header from "./component/Header";
-import styles from "../styles/index.module.css";
 import { useTheme } from "../contexts/ThemeContext";
 import Footer from "./component/Footer";
 export default function Home({}) {
@@ -15,8 +14,8 @@ export default function Home({}) {
       <br />
       2021年結成。ボーカル・TTとトラックメイカー・mumumutaの2人組テクノポップバンド。
       「テクノやジャズ、ロックを中心とした音楽的背景から生まれたダンサブルなサウンド」と
-      「唯一無二のファニーな言葉づかいで構築された歌詞」の組み合わせで”陽気に踊れるダンサブル・ファニー・J-POP”を実現する。<br />
-      作詞作曲をはじめ、ミックス・マスタリング・アートワークなどすべての工程を自分たちで行う"完全インディペンデントアーティスト"である。<br />
+      「唯一無二のファニーな言葉づかいで構築された歌詞」の組み合わせで&ldquo;陽気に踊れるダンサブル・ファニー・J-POP&ldquo;を実現する。<br />
+      作詞作曲をはじめ、ミックス・マスタリング・アートワークなどすべての工程を自分たちで行う&ldquo;完全インディペンデントアーティスト&ldquo;である。<br />
       2022年にリリースした『明け方 (feat. CO2)』は、Spotify公式プレイリスト『キラキラポップ：ジャパン』にも選出された。<br />
       最新リリースは2ndアルバム『チェイス！！！』で、1曲目の『休日』はmumumutaの音楽的個性がパーカッション部分に発揮された、<br />
       テクニカルな1曲になっている。<br />
